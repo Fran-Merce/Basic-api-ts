30e21cceb878a66de1defb82a7090667bd1d6fe0
fran-merce <franmerce1@gmail.com>
2022-10-06 15:17:24 -0300

    fix-build

--- dist/routes/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const auth_1 = require("../controllers/auth");
-exports.router = (0, express_1.Router)();
-exports.router.post('/register', auth_1.registerCtrl);
-exports.router.post('/login', auth_1.loginCtrl);
